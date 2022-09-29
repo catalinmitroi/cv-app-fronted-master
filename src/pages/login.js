e308fc3d1e19b453b3f3e8e087d984986ab8c42b
catalinmitroi <catalinmitroi18@yahoo.com>
2022-09-29 09:44:29 +0300

    Login with MUI

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,34 +1,60 @@
-import React from "react";
+import * as React from "react";
 import "./login.css";
-function LogIn() {
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function Login() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App1">
-      <div className="App">
-        <div className="align-center">
-          <div className="form">
-            <div className="form-inner">
-              <h2>Autentificare</h2>
-              <div className="form-group">
-                <input type="text" placeholder="Username..." />
-              </div>
-              <div className="form-group">
-                <input type="password" placeholder="Password..." />
-              </div>
-              <div>
-                <hr></hr>
-                <input type="submit" value="Conecteaza-te" />
-                <a href="/">
-                  <button>
-                    <h1>Ai uitat parola?</h1>
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <p>Informatii diverse...</p>
+      <Button variant="outlined" onClick={handleClickOpen} id="btnLogIn">
+        Open login form
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle align="center">Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Pentru logare trebuie sa introduceti username si parola.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="username"
+            label="Username"
+            type="username"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
-
-export default LogIn;
